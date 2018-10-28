65fc1541cfaefac370aaf2f39a3096db4c58fff4
Maxwell DeVos <maxdevos49@gmail.com>
2018-10-28 00:35:50 -0500

    im dumb

--- GameJamFall2018/server.js
+++ GameJamFall2018/server.js
@@ -3,14 +3,13 @@ const app = express();
 const http = require('http');
 const ip = require('ip');
 const routes = require('./routes.js');
-// const path = require('path');
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8080;
 
 //view engine
 app.set('view engine', 'vash');
-app.set('views', '/views');
+app.set('views', __dirname + '/views');
 
 //set the routes for the server to use
 app.use('/', routes);
