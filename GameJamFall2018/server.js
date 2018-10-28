6e448e25c257732689cad6ddfc5251b1622bdcba
Maxwell DeVos <maxdevos49@gmail.com>
2018-10-28 00:01:55 -0500

    test

--- GameJamFall2018/server.js
+++ GameJamFall2018/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 //view engine
 app.set('view engine', 'vash');
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 
 //set the routes for the server to use
 app.use('/', routes);
